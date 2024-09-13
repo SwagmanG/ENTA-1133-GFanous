a62e3f40ddaf1c9e1185c633dc5c86e7aaa31c8b
SwagmanG <fanousgoran@gmail.com>
2024-09-13 10:56:23 -0700

    Readded initial commit

---
+++ Lab-One-File.cs
@@ -0,0 +1,16 @@
+﻿namespace GD12_1133_Lab1_Fanous_Goran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!! :) ");
+            Console.WriteLine("My name is Goran.");
+            Console.WriteLine("The date is September 6th 2024.");
+            Console.WriteLine("Decimal 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
+            Console.WriteLine("Binary 0, 1, 10, 100, 1000, 10000, 100000, 1000000, 1100100, 11111111");
+            Console.WriteLine("Hexadecimal 0, 1, 2, 4, 8, 10, 20, 40, 64, ff");
+            Console.WriteLine("Goodbye!!! Have a goood day!!!");
+        }
+    }
+}
