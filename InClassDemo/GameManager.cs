1647bbe731ec9b49a8a4949e57cdd3c098d8b29c
SwagmanG <fanousgoran@gmail.com>
2024-09-15 13:16:16 -0700

    Completed the Dice roller game, added comments to explain, and fixed some broken code.

--- InClassDemo/GameManager.cs
+++ InClassDemo/GameManager.cs
@@ -12,32 +12,38 @@ namespace InClassDemo
 
         internal void PlayGame()
         {
-
+            
             Console.WriteLine("Welcome to Die vs Die!");
-
+            //Assigns class RollDie to a variable.
             RollDie diceRollInstance;
             diceRollInstance = new RollDie();
-
+            //Calls the description of the dice roller from the RollDie class.
             diceRollInstance.Description();
-            //WhenPointGained();
-            //dieroller1 dierollerinstance
+            
             Console.WriteLine("Let's roll!");
-
-            diceRollInstance.Rolling();
-
-        }
-
-
-        private void WhenPointGained()
+            //Calls the RollScore function from the RollDie class.
+            diceRollInstance.RollScore();                          
+        }  
+        
+        internal void ArithmeticOperator()
         { 
+            /*This is where Ill explain the arithmetic operators.
+             *The ++ operator will increment an operand by one.
+             *The -- operator will decrement an operand by one.
+             *The * operator will multply its operands.
+             *The / operator will divide its operands.
+             *The + operator will add the sum of its operands.
+             *The - operator will subtract the left operand by the right operand.
+             *The % will compute the remainder after dividing the left operand by the right operand.
+             *The < operator returns true if its left operand is less than the right operand otherwise, false.
+             *The > operator returns true if its left operand is greater than the right operand otherwise, false.
+             *The <= operator is the same as the < operator but it will also return true if the operands are equal.
+             *The >= operator is the same as the > operator but it will also return true if the operands are equal.
+            */
         
-            score++;
-            string messageReceived = "You gained a point!";
-            Console.WriteLine(messageReceived);
-            Console.WriteLine(score);
+        
         }
-        
-    }
 
+    }   
 }   
     
--- InClassDemo/Program.cs
+++ InClassDemo/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //Calls a function from the GameManager class.
             GameManager manager = new GameManager();
             manager.PlayGame();
         }
