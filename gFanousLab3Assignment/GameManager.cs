a4f07dc1c2644e3bd24740246a04804cf2e01892
SwagmanG <fanousgoran@gmail.com>
2024-09-20 15:28:06 -0700

    Cloned Dice roller and added more die types.

---
+++ gFanousLab3Assignment/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gFanousLab3Assignment
+{
+    internal class GameManager
+    {
+        internal void PlayGame()
+        {
+            Console.WriteLine("Goran,  2024-09-16");
+            Console.WriteLine("Welcome to Die vs Die!");
+
+        }
+    }
+}
---
+++ gFanousLab3Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace gFanousLab3Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calls a function from the GameManager class.
+            GameManager manager = new GameManager();
+            manager.PlayGame();
+        }
+    }
+}
